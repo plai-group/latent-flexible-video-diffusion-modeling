fd06bf60ae5c4683f83670cc1095654110d43071
Jinsoo Yoo <jasony97@alexandria.cs.ubc.ca>
2024-11-06 15:47:52 -0800

    update metrics_plot.py script

--- scripts/metrics_plot.py
+++ scripts/metrics_plot.py
@@ -1,30 +1,50 @@
 import argparse
+import glob
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
+def setup_matplotlib():
+    nice_fonts = {
+        # Use LaTeX to write all text
+        "text.usetex": True,
+        "text.latex.preamble": r"\usepackage{amsfonts}",
+        "font.family": "serif",
+        # Use 10pt font in plots, to match 10pt font in document
+        "axes.labelsize": 10,
+        "font.size": 10,
+        # Make the legend/label fonts a little smaller
+        "legend.fontsize": 8,
+        "xtick.labelsize": 7,
+        "ytick.labelsize": 7,
+    }
+    plt.rcParams.update(nice_fonts)
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Plot metrics from CSV files.')
-    parser.add_argument('--csv_paths', nargs='+', type=str, required=True,
-                                       help='Paths to the CSV files. Must be given in the order of ascending training epochs.',)
-    parser.add_argument('--nicknames', nargs='+', type=str, default=None,
-                                       help='If specified, only plot runs with these nicknames.',)
-    parser.add_argument('--out_path', type=str, help='Where to save the CSV file.', default="metrics_plot.png")
+    parser.add_argument('--csv_path_prefix', type=str, required=True,
+                        help='Path prefix to the CSV files.',)
+    parser.add_argument('--output_path', type=str, help='Directory to save the PNG file.', default="plots/default/plot.png")
     return parser.parse_args()
 
-def plot_metrics(csv_paths, out_path, nicknames):
-    dfs = [pd.read_csv(path) for path in csv_paths]
-    steps = [int(path.split('/')[-2].split('_')[-1][1:]) for path in csv_paths]
-    metrics = [c for c in dfs[0].columns if c not in ['nickname', 'wandb']]
-    fig, ax = plt.subplots(len(metrics), sharex=True)
+def plot_metrics(csv_dirs, out_path):
+    """
+    csv_paths should have format: <PATH>/<PREFIX>_<FRAME INDEX>/final.csv where <FRAME INDEX> is an integer.
+    """
+    csv_dirs = sorted(csv_dirs, key=lambda e: int(e.split('_')[-1]))  # sort CSVs by timestep index
+    dfs = [pd.read_csv(os.path.join(path, 'final.csv')) for path in csv_dirs]
+    steps = [int(path.split('_')[-1]) for path in csv_dirs]
+    metrics = [c for c in dfs[0].columns if c not in ['nickname', 'wandb'] and not c.endswith('-err')]
+    dfs = [df.rename(columns={metric: metric.rstrip('-') for metric in metrics}) for df in dfs]
+    fig, ax = plt.subplots(1, len(metrics), sharex=True, figsize=(16,4))
 
     for mid, metric in enumerate(metrics):
+        metric = metric.rstrip('-')
         all_metric_info = dfs[0][['nickname', metric]].rename(columns={metric: f"{metric}_{steps[0]}"})
         for step, df in zip(steps[1:], dfs[1:]):
             metric_info = df[['nickname', metric]].rename(columns={metric: f"{metric}_{step}"})
             all_metric_info = all_metric_info.merge(metric_info, on='nickname')
-        if nicknames is not None:
-            all_metric_info = all_metric_info[all_metric_info['nickname'].isin(nicknames)]
         for nickname in all_metric_info['nickname']:
             x_axis = [i for i in range(len(steps))]
             y_axis = all_metric_info[all_metric_info['nickname']==nickname].drop(columns='nickname').values.flatten().tolist()
@@ -35,13 +55,15 @@ def plot_metrics(csv_paths, out_path, nicknames):
 
     handles, labels = ax[-1].get_legend_handles_labels()
     fig.legend(handles, labels, loc='upper right')
-
     plt.tight_layout()
     plt.savefig(out_path)
 
 def main():
     args = parse_args()
-    plot_metrics(args.csv_paths, args.out_path, args.nicknames)
+    os.makedirs(os.path.dirname(args.output_path), exist_ok=True)
+    setup_matplotlib()
+    csv_dirs = glob.glob(args.csv_path_prefix + "*")
+    plot_metrics(csv_dirs, args.output_path)
 
 if __name__ == '__main__':
     main()
